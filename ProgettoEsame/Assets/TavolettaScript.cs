bad34db19cd35922009f58303c00197ac5d431fa
federicocolombo12 <126761702+federicocolombo12@users.noreply.github.com>
2024-10-29 12:33:51 +0100

    WC import and Anim

---
+++ ProgettoEsame/Assets/TavolettaScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TavolettaScript : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetTrigger("PulsanteCliccato");
+        }
+    }
+}
