0b65e2074238e23a29e891545fc5abb2cf70e832
federicocolombo12 <126761702+federicocolombo12@users.noreply.github.com>
2025-01-15 12:52:21 +0100

    Update TitleScreenManager.cs

--- ProgettoEsame/Assets/TitleScreenManager.cs
+++ ProgettoEsame/Assets/TitleScreenManager.cs
@@ -24,4 +24,9 @@ public class TitleScreenManager : MonoBehaviour
             });
         
     }
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
 }
