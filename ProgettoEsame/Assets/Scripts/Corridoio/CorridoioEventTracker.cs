a8a0944307dafd7f3761eac0c272f8fbaf68ecab
federicocolombo12 <chiccoclub5@gmail.com>
2025-01-23 19:08:27 +0100

    event system del corridoio

---
+++ ProgettoEsame/Assets/Scripts/Corridoio/CorridoioEventTracker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CorridoioEventTracker : MonoBehaviour
+{
+    public UnityEvent[] corridoioEvents;
+    [SerializeField] private int currentEventIndex = 0;
+    [SerializeField] private int eventsNumber = 4;
+
+    
+    
+
+    // Update is called once per frame
+    
+}
---
+++ ProgettoEsame/Assets/Scripts/Corridoio/TriggerEvent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    CorridoioEventTracker eventTracker;
+    [SerializeField] private int eventIndex;
+    private void Start()
+    {
+        eventTracker = FindObjectOfType<CorridoioEventTracker>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        eventTracker.corridoioEvents[eventIndex].Invoke();
+        Destroy(gameObject);
+    }
+}
