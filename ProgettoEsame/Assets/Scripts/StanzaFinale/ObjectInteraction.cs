187925f145b8c93185e0871b26537fcb704cad6a
AlbertoFulcheri30102 <alby30102@gmail.com>
2025-01-31 18:46:14 +0100

    StanzaFinaleInizio

---
+++ ProgettoEsame/Assets/Scripts/StanzaFinale/ObjectInteraction.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    public Animator animator; // Assegna l'Animator dell'oggetto animato
+    private bool hasActivated = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !hasActivated) // Puoi cambiare la condizione di attivazione
+        {
+            animator.SetTrigger("ActivateAction");
+            hasActivated = true; // Evita che venga attivato più volte
+        }
+    }
+}
