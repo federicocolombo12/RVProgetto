2d7b227a7441eda8792aa56cdf1a5965638e5982
federicocolombo12 <chiccoclub5@gmail.com>
2025-02-01 11:43:38 +0100

    Suonato Audio Attestato

--- ProgettoEsame/Assets/PlayAudioInteract.cs
+++ ProgettoEsame/Assets/PlayAudioInteract.cs
@@ -6,17 +6,35 @@ public class PlayAudioInteract : MonoBehaviour, IInteractable
 {
     // Start is called before the first frame update
     [SerializeField] AudioSource audioSource;
+    bool played = false;
+    int counter = 0;
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
     public void Interact(GameObject interactor)
     {
-        Debug.Log("Interacting with audio");
-        audioSource.Play();
+       
+        
+        Debug.Log("Interacting with " + gameObject.name);
+        if (!audioSource.isPlaying && !played&&counter==0)
+        {
+            gameObject.tag ="Untagged";
+            audioSource.PlayOneShot(audioSource.clip);
+
+            played = true;
+            counter++;
+        }
+        
+        
+       
+        
+           
     }
     public void StopInteract(GameObject interactor)
     {
-        audioSource.Stop();
+        played = false;
     }
+    
+   
 }
