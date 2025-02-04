88cf8117d9af48f21f9371fef5e879c2d80ee0e5
martacolonna27 <127116238+martacolonna27@users.noreply.github.com>
2025-02-04 23:03:29 +0100

    audio inf

--- ProgettoEsame/Assets/Audio/AudioScript/InfermieraBlockingAudio.cs
+++ ProgettoEsame/Assets/Audio/AudioScript/InfermieraBlockingAudio.cs
@@ -5,6 +5,8 @@ public class InfermieraBlockingAudio : MonoBehaviour
     [SerializeField] private AudioSource audioSource;  // AudioSource assegnato manualmente o aggiunto automaticamente
     [SerializeField] private AudioClip firstInteractionClip; // Clip audio per la prima interazione
     [SerializeField] private AudioClip subsequentInteractionClip; // Clip audio per le interazioni successive
+    [SerializeField] private AudioClip nurseWalkingClip; // Clip audio per la camminata dell'infermiera
+
     private InfermieraBlocking infermieraBlocking;
     private int interactionCount = 0; // Conta le interazioni
 
@@ -21,9 +23,7 @@ public class InfermieraBlockingAudio : MonoBehaviour
         audioSource.loop = false;
 
         // Trova il componente InfermieraBlocking
-        
-        
-        
+        infermieraBlocking = GetComponent<InfermieraBlocking>();
     }
 
     // Questo metodo viene chiamato quando inizia l'interazione con l'infermiera
@@ -54,6 +54,27 @@ public class InfermieraBlockingAudio : MonoBehaviour
         if (audioSource.isPlaying)
         {
             audioSource.Stop();
+        }
+    }
+
+    // Metodo per riprodurre il suono della camminata dell'infermiera
+    public void PlayNurseWalking()
+    {
+        if (!audioSource.isPlaying || audioSource.clip != nurseWalkingClip)
+        {
+            audioSource.clip = nurseWalkingClip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    // Metodo per fermare il suono della camminata
+    public void StopNurseWalking()
+    {
+        if (audioSource.isPlaying && audioSource.clip == nurseWalkingClip)
+        {
+            audioSource.Stop();
+            audioSource.loop = false;
         }
     }
 }
