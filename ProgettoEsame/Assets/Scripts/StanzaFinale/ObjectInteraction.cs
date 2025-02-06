417562c9bba2debcfcfa7bbe3e406acb08a17523
AlbertoFulcheri30102 <alby30102@gmail.com>
2025-02-06 14:24:14 +0100

    Animazione e transizione finale + texture

--- ProgettoEsame/Assets/Scripts/StanzaFinale/ObjectInteraction.cs
+++ ProgettoEsame/Assets/Scripts/StanzaFinale/ObjectInteraction.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class ObjectInteraction : MonoBehaviour
 {
     public Animator animator; // Assegna l'Animator dell'oggetto animato
     public bool hasActivated = false;
     private bool isColliding = false; // Variabile per tenere traccia della collisione
-
+    //public GameObject videoObject; // Oggetto che contiene il VideoPlayer
+    //public GameObject canvasObject; // Aggiungi il riferimento al Canva
+    public float delayTime = 2f; // Tempo di attesa per avviare il video
+    public bool startVideo = false;
+/*
+    private void Start()
+    {
+        if (videoObject != null && canvasObject != null)
+        {
+            videoObject.SetActive(false); // Disattiva il video all'inizio
+            canvasObject.SetActive(false); // Disattiva anche il Canvas all'inizio
+        }
+    }
+*/
     void Update()
     {
         if (isColliding && Input.GetKeyDown(KeyCode.E) && !hasActivated)
@@ -16,6 +30,7 @@ public class ObjectInteraction : MonoBehaviour
         else if (isColliding && Input.GetKeyDown(KeyCode.E) && hasActivated)
         {
             StartCoroutine(TriggerActivateAction());
+            //StartVideoAfterDelay();
         }
     }
 
@@ -32,6 +47,7 @@ public class ObjectInteraction : MonoBehaviour
     {
         animator.SetTrigger("ActivateAction");
         hasActivated = false;
+        startVideo = true;
         yield return null; // Puoi cambiare se serve una pausa
     }
 
@@ -50,5 +66,39 @@ public class ObjectInteraction : MonoBehaviour
             isColliding = false;
         }
     }
+    /*
+    private void AvviaVideo()
+    {
+        if (canvasObject != null && videoObject != null && !canvasObject.activeSelf)
+        {
+            canvasObject.SetActive(true); // Attiva il Canvas che contiene il video
+            videoObject.SetActive(true);  // Attiva il GameObject del VideoPlayer (lo rende visibile)
+
+            Debug.Log("Video avviato.");
+
+            VideoPlayer videoPlayer = videoObject.GetComponent<VideoPlayer>();
+            if (videoPlayer != null)
+            {
+                // Avvia il video
+                videoPlayer.Play();
+                Debug.Log("VideoPlayer avviato.");
+            }
+            else
+            {
+                Debug.LogError("Nessun componente VideoPlayer trovato sull'oggetto video.");
+            }
+        }
+    }
+    private void StartVideoAfterDelay()
+    {
+        StartCoroutine(WaitAndStartVideo());
+    }
+
+    // Coroutine per attendere 30 secondi e poi avviare il video
+    private IEnumerator WaitAndStartVideo()
+    {
+        yield return new WaitForSeconds(delayTime); // Attende 30 secondi
+        AvviaVideo(); // Chiama il metodo AvviaVideo
+    }*/
 }
 
