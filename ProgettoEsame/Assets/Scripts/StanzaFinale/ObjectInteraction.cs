6ae4e14cc89b4a1a94c1d7d9f0509342cccf6bf3
martacolonna27 <127116238+martacolonna27@users.noreply.github.com>
2025-02-11 16:16:36 +0100

    suoni ultima stanza

--- ProgettoEsame/Assets/Scripts/StanzaFinale/ObjectInteraction.cs
+++ ProgettoEsame/Assets/Scripts/StanzaFinale/ObjectInteraction.cs
@@ -52,7 +52,7 @@ public class ObjectInteraction : MonoBehaviour
     {
         Debug.Log("Attivazione trigger Disapprova");
         animator.SetTrigger("Disapprova");
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
         animator.SetTrigger("TornaIndietro");
         hasActivated = true;
     }
