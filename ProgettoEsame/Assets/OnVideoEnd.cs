f8a45fc17eaefdd62292445ae986c52dac4ea56e
federicocolombo12 <chiccoclub5@gmail.com>
2025-02-12 20:11:30 +0100

    commit finale per build

---
+++ ProgettoEsame/Assets/OnVideoEnd.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class OnVideoEnd : MonoBehaviour
+{
+    [SerializeField] VideoPlayer videoPlayer; // Assign the VideoPlayer component in the Inspector
+
+    void Start()
+    {
+        ;
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached += OnVideoFinished; // Subscribe to the event
+        }
+        else
+        {
+            Debug.LogError("VideoPlayer component not assigned!");
+        }
+    }
+
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        Debug.Log("Video has ended!");
+        // Add the action you want to perform here
+        ReturnHome();
+    }
+
+    void ReturnHome()
+    {
+        Application.Quit();
+        
+    }
+}
