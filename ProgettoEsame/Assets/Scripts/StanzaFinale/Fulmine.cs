b115599330224888d3862bfeb25daa3721de1bfb
federicocolombo12 <chiccoclub5@gmail.com>
2025-02-12 19:04:44 +0100

    sound effect pioggia

--- ProgettoEsame/Assets/Scripts/StanzaFinale/Fulmine.cs
+++ ProgettoEsame/Assets/Scripts/StanzaFinale/Fulmine.cs
@@ -5,12 +5,14 @@ public class Fulmine : MonoBehaviour
 {
     private Light lightningLight;
     private bool doppio = true;
+    [SerializeField] AudioSource thunderSound;
 
     void Start()
     {
         lightningLight = GetComponent<Light>();
         lightningLight.enabled = false;
         StartCoroutine(ActivateAndDeactivate());
+        thunderSound = GetComponent<AudioSource>();
     }
 
     private IEnumerator ActivateAndDeactivate()
@@ -19,11 +21,13 @@ public class Fulmine : MonoBehaviour
         {
             yield return new WaitForSeconds(Random.Range(5, 15));
             lightningLight.enabled = true;
+            thunderSound.Play();
             yield return new WaitForSeconds(0.2f);
             lightningLight.enabled = false;
             if (doppio) { 
                 yield return new WaitForSeconds(Random.Range(0.01f, 0.3f));
                 lightningLight.enabled = true;
+                thunderSound.Play();
                 yield return new WaitForSeconds(0.2f);
                 lightningLight.enabled = false;
             }
