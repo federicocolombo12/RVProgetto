0403393fdd07cf581742434695fe005ccdf6e790
federicocolombo12 <chiccoclub5@gmail.com>
2025-02-12 10:17:45 +0100

    sistemato pazzo e mappa

--- ProgettoEsame/Assets/TriggerPazzo.cs
+++ ProgettoEsame/Assets/TriggerPazzo.cs
@@ -7,8 +7,9 @@ public class TriggerPazzo : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] PazzoScript pazzoScript;
     private float lastActionTime = -5f; // Inizializza a -5 per permettere la prima esecuzione immediata
-    private const float actionCooldown = 5f; // Intervallo di 5 secondi
+    private const float actionCooldown = 10f; // Intervallo di 5 secondi
     private AudioSource audioSource;
+    private bool played = false;
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();   
@@ -18,7 +19,11 @@ public class TriggerPazzo : MonoBehaviour
         if (Time.time - lastActionTime >= actionCooldown)
         {
             pazzoScript.Silence();
-            audioSource.Play();
+            if (!played)
+            {                 audioSource.Play();
+                played = true;
+            }
+            
             lastActionTime = Time.time;
         }
     }
