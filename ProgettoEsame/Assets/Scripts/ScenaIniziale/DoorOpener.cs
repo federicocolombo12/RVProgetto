6e3e4701029f4139ef6fe7ede3b63bd533b048eb
federicocolombo12 <chiccoclub5@gmail.com>
2025-02-13 21:10:07 +0100

    sistemato tag porta

--- ProgettoEsame/Assets/Scripts/ScenaIniziale/DoorOpener.cs
+++ ProgettoEsame/Assets/Scripts/ScenaIniziale/DoorOpener.cs
@@ -32,7 +32,7 @@ public class DoorOpener : MonoBehaviour
                 
                 return;
             }
-            doorPrefab.tag = "OggettoInteragibile1";
+            
             doorPrefab.transform.GetChild(0).tag = "OggettoInteragibile1";
             doorPrefab.GetComponent<PlayAudioInteract>().enabled = false;
             doorPrefab.GetComponent<AudioSource>().enabled = false;
