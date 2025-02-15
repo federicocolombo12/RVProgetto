ddb2006b09266f9b479ac5010aaba34383351e72
federicocolombo12 <chiccoclub5@gmail.com>
2025-02-15 12:13:01 +0100

    setting del volume in title screen

--- ProgettoEsame/Assets/Scripts/Managers/AudioManager.cs
+++ ProgettoEsame/Assets/Scripts/Managers/AudioManager.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static AudioManager instance { get; private set; }
+    [SerializeField] private Slider volumeSlider; // Assegna lo slider nell'Inspector
 
+    void Start()
+    {
+        // Imposta il valore dello slider al volume salvato (o 1 se non esiste)
+        volumeSlider.value = PlayerPrefs.GetFloat("GameVolume", 1f);
+        AudioListener.volume = volumeSlider.value;
+
+        // Aggiunge l'evento per rilevare cambiamenti nello slider
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("GameVolume", volume); // Salva il volume tra le sessioni
+        PlayerPrefs.Save();
+    }
     private void Awake()
     {
         if (instance != null && instance != this)
